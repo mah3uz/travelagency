522a64cd8717d84fe61605c9636abb6dad733bd3
Mahfuz Shaikh <mah3uz@gmail.com>
2017-12-31 11:39:41 +0600

    Fix start date column

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -23,8 +23,8 @@
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
 <script>
-    $(function() {
-        $( "#datepicker" ).datepicker("option", "dateFormat", 'YYYY-MM-DD');
+    $('#datepicker').datepicker({
+        format: 'yyyy-mm-dd'
     });
 </script>
 
